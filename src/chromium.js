10d227f51c25c638ce5a8b6261c172b52ff6a9c3
mikekrell <michaelkrell@gmail.com>
2020-12-10 15:21:45 -0800

    no idea

--- src/chromium.js
+++ src/chromium.js
@@ -1,21 +1,9 @@
 import {launch} from 'puppeteer-core';
-import {chrome}  from 'chrome-aws-lambda';
-
-const exePath = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome";
-
-async function getOptions () {
-    let options = {
-        args: [],
-        executablePath: await chrome.executablePath,
-        headless: chrome.headless
-    }
-
-    return options
-}
+import { chrome }  from 'chrome-aws-lambda';
 
 export async function getScreenshot (url) {
     const options = await getOptions();
-    const browser = await launch(options);
+    const browser = await launch({ headless : true });
     const page = await browser.newPage();
     await page.setViewport({width: 300, height: 300})
     await page.goto(url)
