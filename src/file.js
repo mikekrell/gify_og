c52b2aa3ab77996a3e872f0ac59d06456480f4b7
mikekrell <michaelkrell@gmail.com>
2020-12-10 14:17:07 -0800

    added second part of file logic

--- src/file.js
+++ src/file.js
@@ -7,4 +7,7 @@ import {promisify} from 'util';
 export function writeTempFile (fileName, html) {
     const hashedFileName = createHash("md5").update(fileName).digest('hex') + ".html";
     console.log(hashedFileName)
+
+    const filePath = join(tmpdir, hashedFileName);
+    console.log(filePath)
 }
