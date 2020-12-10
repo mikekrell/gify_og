9c0b8f8bd2aa0ba2015b76e11ec41aaf3b889ccb
mikekrell <michaelkrell@gmail.com>
2020-12-10 14:19:38 -0800

    added third part of file logic

--- src/file.js
+++ src/file.js
@@ -4,10 +4,14 @@ import {tmpdir} from 'os'
 import {writeFile} from 'fs'
 import {promisify} from 'util';
 
-export function writeTempFile (fileName, html) {
+const promiseWriteFile = promisify(writeFile)
+
+export async function writeTempFile (fileName, html) {
     const hashedFileName = createHash("md5").update(fileName).digest('hex') + ".html";
     console.log(hashedFileName)
 
-    const filePath = join(tmpdir, hashedFileName);
+    const filePath = join(tmpdir(), hashedFileName);
     console.log(filePath)
+
+    await promiseWriteFile(filePath, html);
 }
