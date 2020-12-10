ea92945621f585f1c92a51e25bb526f0a7f3a711
mikekrell <michaelkrell@gmail.com>
2020-12-10 15:46:16 -0800

    refactored with page

--- src/chromium.js
+++ src/chromium.js
@@ -16,8 +16,8 @@ exports.getScreenshot = async (url) => {
         let page = await browser.newPage();
 
         await page.goto(url || 'https://example.com');
-
-        result = await page.title();
+        const screen = await page.screenshot({ type: "png", quality: 70, fullScreen: true });
+        result = await screen;
     } catch (error) {
         return console.error(error);
     } finally {
@@ -27,6 +27,5 @@ exports.getScreenshot = async (url) => {
     }
 
 
-
-    return await page.screenshot({type:"png", quality: 70, fullScreen: true});
+    return result
 };
