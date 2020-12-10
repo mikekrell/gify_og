57dbc6c256cf4ea26381a6932e105b2adad72b87
mikekrell <michaelkrell@gmail.com>
2020-12-10 15:35:50 -0800

    dummy data in screeshot function

--- src/chromium.js
+++ src/chromium.js
@@ -1,11 +1,23 @@
-import {launch} from 'puppeteer-core';
+const puppeteer = require('puppeteer');
 import { chrome }  from 'chrome-aws-lambda';
 
 export async function getScreenshot (url) {
 
-    const browser = await launch({ headless : true });
+    const browser = await puppeteer.launch({ headless : true });
     const page = await browser.newPage();
-    await page.setViewport({width: 300, height: 300})
     await page.goto(url)
-    return page.screenshot({type:"png", quality: 100, fullPage: true })
+    await page.setViewport({ width: 300, height: 300 })
+
+    const dimensions = await page.evaluate(() => {
+        return {
+            width: document.documentElement.clientWidth,
+            height: document.documentElement.clientHeight,
+            deviceScaleFactor: window.devicePixelRatio
+        };
+    });
+
+    console.log('Dimensions:', dimensions);
+
+    return await browser.close(); //page.screenshot({type:"png", quality: 100, fullPage: true })
+
 }
