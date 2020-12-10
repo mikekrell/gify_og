5a490028d0b8ccdb226d9129a4a15c24a1c8599e
mikekrell <michaelkrell@gmail.com>
2020-12-10 13:34:13 -0800

    parseReq

--- src/parseReqs.js
+++ src/parseReqs.js
@@ -4,6 +4,6 @@ export function parseReqs(req) {
     const {query = { } } = parse(req.url || "", true);
     const {title} = query;
     const parseReqs = {title}
-
+    console.log(JSON.stringify(parseReqs))
     return parseReqs;
 }
