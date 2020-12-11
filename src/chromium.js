e3837f78c145dd561e63b78c83a38dd50bb39dc8
mikekrell <michaelkrell@gmail.com>
2020-12-10 17:41:02 -0800

    recorder

--- src/chromium.js
+++ src/chromium.js
@@ -1,4 +1,3 @@
-
 const chromium = require('chrome-aws-lambda');
 const { record } = require('puppeteer-recorder')
 export async function getScreenshot (url) {
@@ -16,17 +15,17 @@ export async function getScreenshot (url) {
         let page = await browser.newPage();
         await page.setViewport({width:300, height: 300});
         await page.goto(url);
-        const screen = await record({
-            browser: browser, // Optional: a puppeteer Browser instance,
-            page: page, // Optional: a puppeteer Page instance,
-            output: 'output.gif',
-            fps: 60,
-            frames: 60 * 5, // 5 seconds at 60 fps
-            prepare: function (browser, page) { /* executed before first capture */ },
-            render: function (browser, page, frame) { /* executed before each capture */ }
-        });
+        // const screen = await record({
+        //     browser: browser, // Optional: a puppeteer Browser instance,
+        //     page: page, // Optional: a puppeteer Page instance,
+        //     output: 'output.gif',
+        //     fps: 60,
+        //     frames: 60 * 5, // 5 seconds at 60 fps
+        //     prepare: function (browser, page) { /* executed before first capture */ },
+        //     render: function (browser, page, frame) { /* executed before each capture */ }
+        // });
 
-        return screen;
+        return await page.screenshot({type:'png', fullScreen: true});
 
     } catch (error) {
         return console.error(error);
