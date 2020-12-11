02063b457c9bf517827173983e6f92e63334c471
mikekrell <michaelkrell@gmail.com>
2020-12-10 17:29:19 -0800

    looper

--- src/canvas.js
+++ src/canvas.js
@@ -7,12 +7,19 @@ export default async function(req, res){
     try{
         const parsedReqs = parseReqs(req);
         const html = getHtml(parsedReqs);
-        const bufferArray = [];
-        let numOfImages = 20;
+
         const filePath = await writeTempFile(parsedReqs.title , html);
         const fileUrl = `file://${filePath}`;
-        const file = await getScreenshot(fileUrl); 
+        const bufferArray = []
+        let file = await getScreenshot(fileUrl);
+        bufferArray.push(file)
+        for(var a =0 ; a<20; a++) {
+            setTimeout(function () {
+                bufferArray.push(await getScreenshot(fileUrl));
+            }, 500);
+        }
 
+        console.log(bufferArray)
         res.statusCode = 200;
         res.setHeader("Content-Type", "image/png");
         res.end(file);
