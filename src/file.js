9ca7bd3835e11ac64065245a524373950ef9aa28
mikekrell <michaelkrell@gmail.com>
2020-12-10 19:40:49 -0800

    testing

--- src/file.js
+++ src/file.js
@@ -1,17 +1,17 @@
-import {createHash} from 'crypto';
-import { join } from 'path';
-import {tmpdir} from 'os'
-import {writeFile} from 'fs'
-import {promisify} from 'util';
+const crypto =  require( 'crypto');
+const path =  require( 'path');
+const os =  require( 'os')
+const fs  =  require( 'fs')
+const util =  require( 'util');
 
-const promiseWriteFile = promisify(writeFile)
+const promiseWriteFile = util.promisify(fs.writeFile())
 
 module.exports.writeTempFile =  async (fileName, html) => {
     const moonLanding = new Date()
 
-    const hashedFileName = createHash("md5").update(fileName + moonLanding.getMilliseconds()).digest('hex') + ".html";
+    const hashedFileName = crypto.createHash("md5").update(fileName + moonLanding.getMilliseconds()).digest('hex') + ".html";
 
-    const filePath = join(tmpdir(), hashedFileName);
+    const filePath = path.join(os.tmpdir(), hashedFileName);
 
     await promiseWriteFile(filePath, html);
 
@@ -21,7 +21,7 @@ module.exports.writeTempFile =  async (fileName, html) => {
 module.exports.writeImageFile = async (fileName, data, ext) => {
     const moonLanding = new Date()
 
-    const hashedFileName = createHash("md5").update(fileName + moonLanding.getMilliseconds()).digest('hex') + `.${ext}`;
+    const hashedFileName = crypto.createHash("md5").update(fileName + moonLanding.getMilliseconds()).digest('hex') + `.${ext}`;
 
     const filePath = join(tmpdir(), hashedFileName);
 
